61791a1860d309fe8bb3c8abc14f0bff7cb293c9
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-09 20:48:55 +0700

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу,которая 
+//на входе принимает число и 
+//на выходе выдает его квадрат.
+
+Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(number * number);
