5bcf15eb5ed7ffa637065eef8c4c4c832d7a3f9f
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-10 09:53:03 +0700

    task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -4,3 +4,16 @@
 //456-> 6
 //782-> 2
 //918-> 8
+
+Console.WriteLine("Введите  трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 100 & number < 1000)
+{
+    Console.Write(number % 10);
+}
+else
+{
+    Console.Write("Введенное число не трехзначное");
+}
+
